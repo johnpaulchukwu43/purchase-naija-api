9edee323478b0d3220579692a992e132af07c252
chukwujohnpaul <jchukwu@teamapt.com>
2019-05-24 01:34:42 +0100

    added authentication for db connection

--- v1/config/secret.js
+++ v1/config/secret.js
@@ -3,8 +3,11 @@ dotenv.config();
 
 module.exports = {
     database:process.env.DATABASE_URL,
+    authDatabase:process.env.AUTH_DATABASE_NAME,
     port: process.env.PORT,
     secretKey: process.env.SECRET_KEY,
     paystackKey:process.env.PAYSTACK_KEY,
-    elasticSearchUrl:process.env.ELASTIC_SEARCH_URL
+    elasticSearchUrl:process.env.ELASTIC_SEARCH_URL,
+    databaseUsername:process.env.DATABASE_USERNAME,
+    databasePassword:process.env.DATABASE_PASSWORD
 };
